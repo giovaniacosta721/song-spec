126e63b466c8faff80d2b978cb89177fdec49987
Giovani Acosta <giovaniacosta.721@gmail.com>
2024-07-19 22:56:34 -0400

    Dependencies changes.

---
+++ src/main/java/com/stationeries/song_spec/SongSpecApplication.java
@@ -0,0 +1,13 @@
+package com.stationeries.song_spec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongSpecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongSpecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stationeries/song_spec/SongSpecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stationeries.song_spec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongSpecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
